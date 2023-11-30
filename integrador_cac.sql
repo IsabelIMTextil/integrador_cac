499e8c7018ade95f0f61a6ed6dc63b11b7552145
IsabelIMTextil <imtextil@gmail.com>
2023-11-29 22:44:01 -0300

    cambio de oradores

--- integrador_cac.sql
+++ integrador_cac.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 05-11-2023 a las 01:05:02
+-- Tiempo de generación: 27-11-2023 a las 00:03:13
 -- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Versión de PHP: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,16 +18,16 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `integrador_cac`
+-- Base de datos: `Oradores_textiles`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `oradores`
+-- Estructura de tabla para la tabla `Oradores`
 --
 
-CREATE TABLE `oradores` (
+CREATE TABLE `Oradores` (
   `id_orador` int(10) NOT NULL,
   `nombre` varchar(30) NOT NULL,
   `apellido` varchar(30) NOT NULL,
@@ -37,29 +37,29 @@ CREATE TABLE `oradores` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Volcado de datos para la tabla `oradores`
+-- Volcado de datos para la tabla `Oradores`
 --
 
-INSERT INTO `oradores` (`id_orador`, `nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES
-(1, 'Pericles', 'Griego', 'peri_aC@gmail.com', '495 - 429 a.C. El secreto de la felicidad es la libertad. Y el secreto de la libertad es el coraje.', '2023-11-06'),
-(2, 'Demóstenes', 'de Atenas', 'demo@gmail.com', '384 - 322 aC. Cuando una batalla está perdida, sólo los que han huido peden combatir en otra.', '2023-11-07'),
-(3, 'Abraham ', 'Lincoln', 'brahma@gmail.com', '1809 - 1865. El gobierno del pueblo, por el pueblo y para el pueblo no desaparecerá de la tierra.', '2023-11-08'),
-(4, 'Winston', 'Churchill', 'debil@gmail.com', '1874 - 1965. Nunca llegarás a tu destino si te paras a lanzar piedras a cada perro que ladre.', '2023-11-09'),
-(5, 'Mahatma', 'Gandhi', 'gandhi@gmail.com', '1869 - 1948. Ojo por ojo y todo el mundo quedará ciego.', '2023-11-10'),
-(6, 'John F.', 'Kennedy', 'johnf@gmail.com', '1917 - 1963. No preguntes qué puede hacer tu país por tí, sino qué puedes hacer tú por tu país.', '2023-11-11'),
-(7, 'Nelson', 'Mandela', 'mandela@gmail.com', '1918 - presente. La educación es el arma más poderosa que puedes usar para cambiar el mundo.', '2023-11-12'),
-(8, 'Martin Luther', 'King Jr.', 'mlking@gmail.com', '1929-1968. Mientras haya pobreza en este mundo, ningún hombre puede ser totalmente rico, incluso si tiene un billón de dólares.', '2023-11-13'),
-(9, 'Miguel Ángel', 'Cornejo', 'macornejo@gmail.com', '1946 - 2015. Investigación Omega', '2023-11-14'),
-(10, 'Daniel', 'Colombo', 'colombod@gmail.com', '1981 - presente. Cómo hablar en público.', '2023-11-15');
+INSERT INTO `Oradores` (`id_orador`, `nombre`, `apellido`, `mail`, `tema`, `fecha_alta`) VALUES
+(1, 'María del Carmen', 'Toribio', 'mct@gmail.com', 'De Ing. Juárez, Formosa. Charla sobre el Arte Textil Ancestral con la fibra del chaguar y difusión de su cultura.', '2023-12-14'),
+(2, 'Ermelinda', 'Painequeo', 'erpaine@gmail.com', 'De Trelew, Chubut. Charla sobre la cultura la cosmovisión y el tejido Mapuche.', '2023-12-14'),
+(3, 'Jesús', 'Casimiro', 'jesus@gmail.com', 'De Luracatao, Salta. Charla sobre el Arte Textil de los Valles Calchaquíes y la cultura Diaguita.', '2023-12-14'),
+(4, 'Mario', 'Vucetich', 'mariov@gmail.com', 'De Buenos Aires. El tejido urbano, técnica en telar de cuadros.', '2023-12-14'),
+(5, 'Jorge', 'Marí', 'jmari@gmail.com', 'De Buenos Aires. Charla sobre textiles de uso tradicional en Iberoamérica: el poncho mapuche.', '2023-12-14'),
+(6, 'Dardo', 'Gonza', 'dardog@gmail.com', 'De Salta. Charla sobre el poncho salteño, estructura del tejido en el telar criollo.', '2023-12-14'),
+(7, 'Margarita', 'Ariza', 'marari@gmail.com', 'El Cercado, Tucumán. Charla sobre la Randa Tucumana, tejido con aguja.', '2023-12-14'),
+(8, 'Leónidas', 'Ñandutí', 'leoduti@gmail.com', 'De Buenos Aires. Charla sobre el Ñandutí, la técnica y su origen.', '2023-12-14'),
+(9, 'Sonia', 'Vera', 'svera@gmail.com', 'De Buenos Aires. Hilado de fibras naturales. Las diversas fibras vegetales, sus orígenes y tratamiento.', '2023-12-14'),
+(10, 'María', 'Cicchitti', 'mcicchitti@gmail.com', 'De Luján de Cuyo, Mendoza. Charla sobre el tejido con personas de la comunidad Huarpe.', '2023-12-14');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `oradores`
+-- Indices de la tabla `Oradores`
 --
-ALTER TABLE `oradores`
+ALTER TABLE `Oradores`
   ADD PRIMARY KEY (`id_orador`);
 
 --
@@ -67,10 +67,10 @@ ALTER TABLE `oradores`
 --
 
 --
--- AUTO_INCREMENT de la tabla `oradores`
+-- AUTO_INCREMENT de la tabla `Oradores`
 --
-ALTER TABLE `oradores`
-  MODIFY `id_orador` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+ALTER TABLE `Oradores`
+  MODIFY `id_orador` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
